592014b07d10909ff4610df9a4d85e503084da76
Thougaard02 <Rasmus@vinperlen.dk>
2022-06-19 15:23:55 +0200

    Added sql tables and SP

---
+++ Airport/SQL/Stored Procedure/StoredProcedure.sql
@@ -0,0 +1,71 @@
+﻿CREATE OR ALTER PROCEDURE SP_InsertIntoAirport 
+	@IATA VARCHAR(3),
+	@Airport_Name VARCHAR(64),
+	@Airport_Address VARCHAR(256)
+AS
+BEGIN
+	SET NOCOUNT ON;
+	INSERT INTO AirPort VALUES(@IATA, @Airport_Name, @Airport_Name)
+END
+GO
+
+CREATE OR ALTER PROCEDURE SP_InsertIntoCompany 
+	@Company_Name VARCHAR(256)
+AS
+BEGIN
+	SET NOCOUNT ON;
+	INSERT INTO Company VALUES(@Company_Name)
+END
+GO
+ 
+CREATE OR ALTER PROCEDURE SP_GetAllAirportIATA
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SELECT AirPort.IATA FROM AirPort 
+END
+GO
+
+CREATE OR ALTER PROCEDURE SP_CreateRoute 
+
+	@Company_Name VARCHAR(64)
+AS
+BEGIN
+	DECLARE @Company_Id INT
+	SET NOCOUNT ON;
+	SET @Company_Id = (SELECT Company.Company_Id FROM Company WHERE Company.Company_Name = @Company_Name);
+
+	INSERT INTO Route VALUES(@Company_Id);
+	
+END
+GO
+
+CREATE OR ALTER PROCEDURE SP_CreateAirportRoute
+	@IATA VARCHAR(3),
+	@Company_Name VARCHAR(64)
+AS
+BEGIN
+	DECLARE @Route_Id INT
+	DECLARE @Company_Id INT
+	SET NOCOUNT ON;
+	
+	SET @IATA = (SELECT AirPort.IATA FROM AirPort WHERE AirPort.IATA = @IATA);
+	
+	SET @Company_Id = (SELECT Company.Company_Id FROM Company WHERE Company.Company_Name = @Company_Name);
+
+	SET @Route_Id = (SELECT Route.Route_Id FROM Route WHERE Route.Company_CompanyId = @Company_Id)
+
+	INSERT INTO AirPort_Route VALUES(@IATA, @Route_Id);
+END
+GO
+
+-- EXEC
+--EXEC SP_InsertIntoAirport 'CPH', 'Copenhagen', 'Bomba' 
+
+--EXEC SP_InsertIntoCompany 'SAS';
+
+--EXEC SP_CreateRoute 'SAS';
+
+--EXEC SP_CreateAirportRoute 'CPH', 'SAS';
+
+--EXEC SP_GetAllAirportIATA
---
+++ Airport/SQL/Tabels/Tables.sql
@@ -0,0 +1,36 @@
+﻿CREATE TABLE Company
+(
+	Company_Id int IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	Company_Name Varchar(256) UNIQUE
+);
+CREATE TABLE Route
+(
+	Route_Id int IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	Company_CompanyId int,
+	FOREIGN KEY (Company_CompanyId) REFERENCES Company(Company_Id)
+);
+CREATE TABLE AirPort
+(
+	IATA VARCHAR(3) PRIMARY KEY NOT NULL,
+	Airport_Name VARCHAR(256) NOT NULL,
+	Airport_Address VARCHAR(256) NOT NULL,
+	CONSTRAINT CH_IATA CHECK (DATALENGTH(IATA) = 3 AND (upper(IATA) = IATA collate Latin1_General_BIN2))
+);
+CREATE TABLE Plane
+(
+	Serial_Number VARCHAR(256) PRIMARY KEY,
+	Company_CompanyId int NOT NULL,
+	Route_RouteId int NOT NULL,
+	Airport_IATA VARCHAR(3) NOT NULL,
+	FOREIGN KEY (Company_CompanyId) REFERENCES Company(Company_Id),
+	FOREIGN KEY (Route_RouteId) REFERENCES Route(Route_Id),
+	FOREIGN KEY (Airport_IATA) REFERENCES Airport(IATA)
+);
+CREATE TABLE Airport_Route
+(
+	Airport_IATA VARCHAR(3) NOT NULL,
+	Route_RouteId INT NOT NULL,
+	PRIMARY KEY(Airport_IATA, Route_RouteId),
+	FOREIGN KEY (Route_RouteId) REFERENCES Route(Route_Id),
+	FOREIGN KEY (Airport_IATA) REFERENCES Airport(IATA)
+);
